6da4f0cb2ee3cdcd1af71a543e8e6426406fcf1e
khoahuynh93 <khoahuynh.ld@gmail.com>
2020-03-24 18:56:25 -0400

    Remove while() in DMX_data_stream.c
    avoid any blocking loop

--- Src/DMX_data_stream.c
+++ Src/DMX_data_stream.c
@@ -14,9 +14,9 @@
  *
  *      				MIN		|	TYP		| 	MAX		|	Unit	|	Logic
  *      		BREAK :	88		|	88		|	1000000	|	usec	|	Low
- *      		MAB   :			|	8		|			|	usec	|	High
- *      FRAME WIDTH	  :			|	44		|			|	usec	|	L/H
- *    START/STOP/DATA :			|	4		|			|	usec	|	L/H
+ *				MAB   :			|	8		|			|	usec	|	High
+ *		FRAME WIDTH	  :			|	44		|			|	usec	|	L/H
+ *	START/STOP/DATA	  :			|	4		|			|	usec	|	L/H
  *    			MTBF  :	0		|	NS		|	1000000 |	usec	|	High
  *    			MTBP  : 0		|	NS		|	1000000 |	usec	|	High
  *    			*NS = Non specify
@@ -29,17 +29,19 @@
 
 uint8_t IBM = 0; // 0 - Idle state ; 1 - BREAK state ; 2 - MAB state
 uint8_t IBM_done = 0; // IBM not done yet
+GPIO_InitTypeDef USART1IO_param;
 
 /* This function is used to set the value of a DMX channel when called */
 void DMX_Write(DMX512_HandleTypeDef *hDMX512, uint8_t dmx_channel,uint8_t dmx_value)
 {
 	hDMX512->DATA_frame[dmx_channel] = dmx_value;
 }
-/* This function will generate IBM sequence on TX pin
+/* This function is implemented in HAL_TIM_PeriodElapsedCallback
+ * This function will generate IBM sequence on TX pin
  * [Inter-packet Idle(MTBP) - BREAK - MARK after BREAK(MAB)] sequence to start a new packet
  * Set MTBP = 600 usec - HIGH
  * Set BREAK = 100 usec	- LOW
- * Set MAB = 18 usec
+ * Set MAB = 12 usec - HIGH
  * @param: TIM_HandleTypeDef with TIM clk at 12Mhz / Prescaler 0 / BASE mode
  * */
 void IBM_Start(DMX512_HandleTypeDef *hDMX512)
@@ -57,47 +59,54 @@ void IBM_Start(DMX512_HandleTypeDef *hDMX512)
 		IBM = 2;
 		break;
 	case 2:
-		HAL_GPIO_WritePin(hDMX512->GPIOx,hDMX512->GPIO_Pin,GPIO_PIN_SET);
 		IBM = 0;
-//		HAL_TIM_Base_Stop_IT(hDMX512->htim); // Stop TIM - End IBM - begin sending DMX FRAMEs
-		TIM4->CR1 = 0;
+		HAL_TIM_Base_Stop_IT(hDMX512->htim);
+		IBM_done = 1;// TIM Stopped - IBM completed
+		HAL_GPIO_WritePin(hDMX512->GPIOx,hDMX512->GPIO_Pin,GPIO_PIN_SET);
+		DMX_Start(hDMX512); // Jump back to DMX_Start to sent DATA frames
 		break;
 	default:
 		break;
 	}
 }
-
+/*	This function is implemented in HAL_UART_TxCpltCallback
+ *	This function control sending IBM sequence and DATA frames
+ *	This function has 2 lines are specific for STM32F103C8T6
+ *	needed to correct those lines for another controller
+ *	That will increase MAB time if SYSCLK < 48MHZ
+ * @param: TIM_HandleTypeDef with TIM clk at 12Mhz / Prescaler 0 / BASE mode
+ * */
 void DMX_Start(DMX512_HandleTypeDef *hDMX512)
 {
-	/*Start of mode switching*/
-	GPIO_InitTypeDef USART1IO_param;
-	USART1IO_param.Pin = hDMX512->GPIO_Pin;
-	USART1IO_param.Mode = GPIO_MODE_OUTPUT_PP;
-	USART1IO_param.Pull = GPIO_NOPULL;
-	USART1IO_param.Speed = GPIO_SPEED_FREQ_MEDIUM;
 
-	HAL_GPIO_Init(hDMX512->GPIOx,&USART1IO_param);
-	/*End of mode swithching*/
-	while(hDMX512->huart->gState != HAL_UART_STATE_BUSY_TX) //Stay here until TX start success - TX busy
+	if(IBM_done)
 	{
-		/*Start sequence begin*/
-		if(!IBM_done)
+		/*Start of IO mode switching*/
+		GPIOB->CRL |= 1U << 27U;
+		GPIOB->CRL |= 1U << 24U;
+		/* Implement this if not STM32F103C8T6
+		 * USART1IO_param.Mode = GPIO_MODE_AF_PP;
+		 * HAL_GPIO_Init(hDMX512->GPIOx,&USART1IO_param);
+		 */
+
+		/*End of mode swithching*/
+		if(HAL_UART_Transmit_IT(hDMX512->huart,hDMX512->DATA_frame,sizeof(hDMX512->DATA_frame)) != HAL_OK)
 		{
-			HAL_TIM_Base_Start_IT(hDMX512->htim); // Start TIM and implement PACKET_start IN ISR
-			IBM_done = 1; // set Start SEQ flag done
+			Error_Handler();
 		}
-		if(TIM4->CR1 == 0) // check when the IBM is done - TIM4 is disable is OK
-		{
-			/*Start of mode switching*/
-			GPIOB->CRL |= 1U << 27U;
-			GPIOB->CRL |= 1U << 24U;
-			/*End of mode swithching*/
-			if(HAL_UART_Transmit_IT(hDMX512->huart,hDMX512->DATA_frame,sizeof(hDMX512->DATA_frame)) != HAL_OK)
-			{
-				Error_Handler();
-			}
-			IBM_done = 0; // reset Start SEQ flag
-		}
+		IBM_done = 0; // reset Start SEQ flag
+	}
+	if((!IBM_done) && (TIM4->CR1 == 0) && (hDMX512->huart->gState != HAL_UART_STATE_BUSY_TX))
+	{
+		/*Start of IO mode switching*/
+		USART1IO_param.Pin = hDMX512->GPIO_Pin;
+		USART1IO_param.Mode = GPIO_MODE_OUTPUT_PP;
+		USART1IO_param.Pull = GPIO_NOPULL;
+		USART1IO_param.Speed = GPIO_SPEED_FREQ_MEDIUM;
+		/*End of mode swithching*/
+
+		HAL_GPIO_Init(hDMX512->GPIOx,&USART1IO_param);
+		HAL_TIM_Base_Start_IT(hDMX512->htim); // Start TIM and implement PACKET_start IN ISR
 
 	}
 }
